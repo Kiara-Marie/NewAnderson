28f038147107b67f2fcfbe918bd53819de2b25cd
r0e1b <kiarag@alumni.ubc.ca>
2019-03-24 22:43:07 -0700

    With about files, still no ipr

---
+++ aboutRun.h
@@ -0,0 +1,28 @@
+#ifndef _ABOUTRUN_H_
+#define _ABOUTRUN_H_
+#include <iostream>
+#include <math.h>
+#include <armadillo>
+#include <string>
+#include <fstream>
+#include <ostream>
+#include "utils.h"
+
+using namespace std;
+using namespace arma;
+
+class AboutRun : public metric {
+	private:
+		int W;
+		int MAXT;
+		int numSites;
+		int iterations;
+		string jMethod;
+	public:
+		AboutRun(int W,int MAXT,int numSites,int iterations,string jMethod);
+		void save(const vec &eigval,const mat &eigvec,const mat &A, int iterations);
+		void printResult();
+
+};
+
+#endif
